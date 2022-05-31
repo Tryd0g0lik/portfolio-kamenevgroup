05d2b5acd6f3449d672f3b22188023ce296d85fd
Tryd0g0lik <work80@mail.ru>
2022-05-31 17:40:20 +0700

    edits

--- themes/custom/kamenev/js/correct.js
+++ themes/custom/kamenev/js/correct.js
@@ -1,3 +1,7 @@
+$(document).ready(function(){
+	let width_gallary =  $('.portfolio__protfoilio div:first-child').width()
+	$('.portfolio__protfoilio > div').css('height', width_gallary);
+});
 
 $(window).resize (function() {
 	let width_gallary =  $('.portfolio__protfoilio div:first-child').width()
